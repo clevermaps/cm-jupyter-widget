0e1b01578976b5a433bfc1ec0f6c1a287d243312
Karel Psota <karel.psota@clevermaps.io>
2024-10-07 19:59:02 +0200

    package dir

--- cm_jupyter_widget/widget.py
+++ cm_jupyter_widget/widget.py
@@ -1,10 +1,11 @@
 import anywidget
 import traitlets
+import pathlib
 
 class CleverMapsWidget(anywidget.AnyWidget):
     
-    _esm = "./index.js"
-    _css = "./index.css"
+    _esm = pathlib.Path(__file__).parent / "index.js"
+    _css = pathlib.Path(__file__).parent / "index.css"
 
     # required
     view_url = traitlets.Unicode(allow_none=False).tag(sync=True)
